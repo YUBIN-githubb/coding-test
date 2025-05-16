2353a5ba313826ab8c275a8104922e75664a77cb
Moon YuBin <yubin5744@gmail.com>
2025-05-16 14:46:47 +0900

    0516 문자열 돌리기

---
+++ 문제/문자열 돌리기.py
@@ -0,0 +1,9 @@
+# 문자열 str이 주어집니다.
+# 문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+
+str = input()
+for char in str:
+    print(char)
+
+# print('\n'.join(input()))
+# 줄바꿈을 구분자로 해서 join() 함수 쓰기
